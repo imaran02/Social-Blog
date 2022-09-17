c89154fa08ff7cc3d19318581d37f383e14ac681
Imaran <Imaran.Asalam@corestack.io>
2022-09-17 10:53:20 +0530

    done with reset email

--- app.py
+++ app.py
@@ -2,15 +2,26 @@ from flask import Flask,render_template,session
 from flask_pymongo import PyMongo,ObjectId
 from flask_bcrypt import Bcrypt
 from flask_session import Session
+from flask_mail import Mail
+
+
+
+
+
+
 # from flask_login import LoginManager
-
+import os
 from flask import Flask, request, abort
-
 app = Flask(__name__)
 app.config['SECRET_KEY']= '5791628bb0b13ce0c676dfde280ba245'
 app.config['MONGO_URI']="mongodb://localhost/sbs"
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'imran.hashmi226@gmail.com'
+app.config['MAIL_PASSWORD'] = 'wjeovbiumczcvnpf'
 mongo = PyMongo(app)
 
 db = mongo.db.userinfo
@@ -19,7 +30,7 @@ bcrypt = Bcrypt(app)
 # login_manager = LoginManager()
 # login_manager.init_app(app)
 sessionv = Session(app)
-
+mail = Mail(app)
 # import declared routes
 import routes
 
--- routes.py
+++ routes.py
@@ -2,14 +2,33 @@ from __main__ import app
 from asyncio.windows_events import NULL
 from datetime import datetime
 import email
+from email import message
 from genericpath import exists
 from unicodedata import name
 from urllib import request
 from flask import render_template,url_for,request,flash,redirect,session
-from app import bcrypt,db,sessionv,postdb
+from app import bcrypt,db,postdb,mail
 import pwnedpasswords
 from bson.objectid import ObjectId
-from flask_login import current_user
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+from flask_mail import Message
+import os
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'imran.hashmi226@gmail.com'
+app.config['MAIL_PASSWORD'] = 'wjeovbiumczcvnpf'
+
+
+
+
+
+import smtplib, ssl
+port = 587
+# # port = 465
+smtp_server = "smtp.gmail.com"
 
 
 
@@ -114,7 +133,6 @@ def delete_post(id):
         flash(f'You are not authorized','danger')
         return redirect(url_for('home'))
     else:
-        print(session['email'])
         postdb.delete_one({'_id':ObjectId(id)})
         flash('Post deleted succesfully','success')
         return redirect(url_for('home'))
@@ -148,3 +166,210 @@ def update_post(id):
             flash('Post updated succesfully','success')
             return redirect(url_for('home'))
     return render_template("updatepost.html",posts=oldvalues)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def get_reset_token(self, expires_sec=1800):
+#     s = Serializer(app.config['SECRET_KEY'], expires_sec)
+#     return s.dumps({'email': self.id}).decode('utf-8')
+
+# @staticmethod
+# def verify_reset_token(token):
+#     s = Serializer(app.config['SECRET_KEY'])
+#     try:
+#         email = s.loads(token)['email']
+#     except:
+#         return None
+#     return session['email']
+
+
+# def send_reset_email(user):
+#     print(user)
+#     s = Serializer(app.config['SECRET_KEY'], 1800)
+#     token  = s.dumps({'email': user}).decode('utf-8')
+#     msg = Message('Password Reset Request',
+#                   sender='immu@icorestack.io',
+#                   recipients=[user])
+#     msg.body = f'''To reset your password, visit the following link:
+# {url_for('reset_token', token=token, _external=True)}
+# If you did not make this request then simply ignore this email and no changes will be made.
+# '''
+#     mail.send(msg)
+
+    # receiver_email = user
+    # message = token
+    # context = ssl.create_default_context()
+    # with smtplib.SMTP(smtp_server, port) as server:
+    #     server.ehlo()  # Can be omitted
+    #     server.starttls(context=context)
+    #     server.ehlo()  # Can be omitted
+    #     server.login(sender_email, password)
+    #     server.sendmail(sender_email, receiver_email, message)
+    #     flash(f'mail sent','success')
+
+# @app.route("/reset_password", methods=['GET', 'POST'])
+# def reset_request():
+#     sender_email = 'imran.hashmi226@gmail.com'
+#     senderpassword = 'wjeovbiumczcvnpf'
+#     if request.method=='POST':
+#         email= request.form['mail']
+#         user = db.find_one({'email':email},{'_id':0,'email':1})
+#         if user is not None:
+#             s = Serializer(app.config['SECRET_KEY'], 1800)
+#             token  = s.dumps({'email': user['email']}).decode('utf-8')
+#             receiver_email = user['email']
+#             # message = {url_for('reset_token', token=token, _external=True)}
+#             message=token
+#             context = ssl.create_default_context()
+#             with smtplib.SMTP(smtp_server, port) as server:
+#                 server.ehlo()  # Can be omitted
+#                 server.starttls(context=context)
+#                 server.ehlo()  # Can be omitted
+#                 server.login(sender_email, senderpassword)
+#                 server.sendmail(sender_email, receiver_email, message)
+#         # context = ssl.create_default_context()
+#         # with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+#         #     server.login(sender_email, senderpassword)
+#         #     server.sendmail(sender_email, receiver_email, message)
+#             flash('An email has been sent with instructions to reset your password.', 'info')
+#             return redirect(url_for('logion'))
+#     return render_template('reset_request.html')
+
+
+# @app.route("/reset_password/<token>", methods=['GET', 'POST'])
+# def reset_token(token):
+#     user = verify_reset_token(token)
+#     if user is None:
+#         flash('That is an invalid or expired token', 'warning')
+#         return redirect(url_for('reset_request'))
+#     form = ResetPasswordForm()
+#     if form.validate_on_submit():
+#         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+#         user.password = hashed_password
+#         flash(f'Your password has been updated! You are now able to log in', 'success')
+#         return redirect(url_for('login'))
+#     return render_template('reset_token.html', form=form)
+
+
+# def send_reset_email(user):
+#     print(user)
+#     s = Serializer(app.config['SECRET_KEY'], 1800)
+#     token  = s.dumps({'email': user}).decode('utf-8')
+# #     msg = Message('Password Reset Request',
+# #                   sender='immu@icorestack.io',
+# #                   recipients=[user])
+# #     msg.body = f'''To reset your password, visit the following link:
+# # {url_for('reset_token', token=token, _external=True)}
+# # If you did not make this request then simply ignore this email and no changes will be made.
+# # '''
+# #     mail.send(msg)
+
+
+# def get_reset_token(self, expires_sec=1800):
+#     s = Serializer(app.config['SECRET_KEY'], expires_sec)
+#     return s.dumps({'email': self.email}).decode('utf-8')
+
+
+@staticmethod
+def verify_reset_token(token):
+    s = Serializer(app.config['SECRET_KEY'])
+    try:
+        email = s.loads(token)['email']
+    except:
+        return None
+    return email
+
+# def send_reset_email(user):
+#     s = Serializer(app.config['SECRET_KEY'], 1800)
+#     token  = s.dumps({'email': user}).decode('utf-8')
+#     msg = Message('Password Reset Request',
+#                   sender='imran.hashmi226@gmail.com',
+#                   recipients=[user])
+#     msg.body = f'''To reset your password, visit the following link:
+# {url_for('reset_token', token=token, _external=True)}
+# If you did not make this request then simply ignore this email and no changes will be made.
+# '''
+#     mail.send(msg)
+
+
+# @app.route("/reset_password", methods=['GET', 'POST'])
+# def reset_request():
+#     if request.method=='POST':
+#         email= request.form['mail']
+#         user = db.find_one({'email':email},{'_id':0,'email':1})
+#         if user is not None:
+#             s = Serializer(app.config['SECRET_KEY'], 1800)
+#             token  = s.dumps({'email': user}).decode('utf-8')
+#             msg = Message('Password Reset Request',
+#                   sender='imran.hashmi226@gmail.com',
+#                   recipients=[user])
+#             msg.body = f'''To reset your password, visit the following link:
+#             {url_for('reset_token', token=token, _external=True)}
+# If you did not make this request then simply ignore this email and no changes will be made.
+# '''
+#             mail.send(msg)
+#         flash('An email has been sent with instructions to reset your password.', 'info')
+#         return redirect(url_for('home'))
+#     return render_template('reset_request.html')
+
+
+
+
+
+
+
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if request.method=='POST':
+        email= request.form['mail']
+        user = db.find_one({'email':email},{'_id':0,'email':1})
+        if user is not None:
+            s = Serializer(app.config['SECRET_KEY'], 1800)
+            token  = s.dumps({'email': user['email']}).decode('utf-8')
+            usere = user['email']
+            msg = Message('Password Reset Request',
+                  sender='imran.hashmi226@gmail.com',
+                  recipients=[usere])
+            msg.body = f'''To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+            mail.send(msg)
+        # context = ssl.create_default_context()
+        # with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        #     server.login(sender_email, senderpassword)
+        #     server.sendmail(sender_email, receiver_email, message)
+            flash('An email has been sent with instructions to reset your password.', 'info')
+            return redirect(url_for('logion'))
+    return render_template('reset_request.html')
+
+
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if session['email'] is not None:
+        return redirect(url_for('home'))
+    user = verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('reset_request'))
+    if request.method=='POST':
+        password = request.form["password"]
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        db.update_one({'email':user},{'$set':{'password':hashed_password}})
+        flash('Your password has been updated! You are now able to log in', 'success')
+        return redirect(url_for('home'))
+    return render_template('reset_token.html')
+
